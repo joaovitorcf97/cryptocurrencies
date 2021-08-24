9690aaab67ab7687d0f3d1b4ed0979c8fd2d5229
João Vitor <joaovitorcfdev@gmail.com>
2021-08-24 00:09:06 -0300

    Selecioando moedas e alterando appBar dinamicamente

--- lib/meu_aplicativo.dart
+++ lib/meu_aplicativo.dart
@@ -10,7 +10,7 @@ class MeuAplicativo extends StatelessWidget {
       title: 'Crypto Currencies',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.indigo,
       ),
       home: MoedasPage(),
     );
--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,30 +1,85 @@
+import 'package:cryptocurrencies/modules/moeda.dart';
 import 'package:cryptocurrencies/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class MoedasPage extends StatelessWidget {
+class MoedasPage extends StatefulWidget {
   const MoedasPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final tabela = MoedaRepository.tabela;
+  _MoedasPageState createState() => _MoedasPageState();
+}
 
+class _MoedasPageState extends State<MoedasPage> {
+  final tabela = MoedaRepository.tabela;
+  NumberFormat real = NumberFormat.currency(locale: 'pt-BR', name: 'R\$');
+  List<Moeda> selecionadas = [];
+
+  appBarDinamica() {
+    if (selecionadas.isEmpty) {
+      return AppBar(
+        title: Text('Crypto Currencies'),
+        centerTitle: true,
+      );
+    } else {
+      return AppBar(
+        elevation: 0,
+        title: Text('${selecionadas.length} moedas selecionadas'),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              selecionadas = [];
+            });
+          },
+        ),
+        iconTheme: IconThemeData(color: Colors.black),
+        textTheme: TextTheme(
+          headline6: TextStyle(color: Colors.black),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Text(
-          'Crypto Currencies',
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-      ),
+      appBar: appBarDinamica(),
       body: ListView.separated(
         itemBuilder: (BuildContext context, int moeda) {
           return ListTile(
-            leading: Image.asset(tabela[moeda].icone),
-            title: Text(tabela[moeda].nome),
-            trailing: Text(tabela[moeda].preco.toString()),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12))),
+            leading: (selecionadas.contains(tabela[moeda]))
+                ? CircleAvatar(
+                    child: Icon(Icons.check),
+                  )
+                : SizedBox(
+                    width: 40,
+                    child: Image.asset(tabela[moeda].icone),
+                  ),
+            title: Text(
+              tabela[moeda].nome,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            trailing: Text(
+              real.format(tabela[moeda].preco),
+            ),
+            selected: selecionadas.contains(tabela[moeda]),
+            selectedTileColor: Colors.indigo[50],
+            onLongPress: () {
+              setState(() {
+                (selecionadas.contains(tabela[moeda]))
+                    ? selecionadas.remove(tabela[moeda])
+                    : selecionadas.add(tabela[moeda]);
+              });
+            },
           );
         },
         separatorBuilder: (_, __) => Divider(),
